359ab285298d9914d07dd7d46aa3a3f7ff90f2f6
Tiago Possato <tiago.possato@ifc-videira.edu.br>
2016-08-22 18:07:20 +0000

    mudando de manquina no c9

--- interface/assets/js/script.js
+++ interface/assets/js/script.js
@@ -1,22 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function() {
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modalAcoes').leanModal();
-  });
+});
 
 
-var log = function (dados, painel) {
+var log = function(dados, painel) {
     if (!painel) {
         console.log(dados);
         return;
     }
-    document.getElementById("areaMensagens").value += dados + "\n";
+
+    $('#areaMensagens').append(dados+"\n");
+    $('#areaMensagens').trigger('autoresize');
+
     document.getElementById("areaMensagens").scrollTop = document.getElementById("areaMensagens").scrollHeight;
 };
 
 function send(msg) {
-    waitForSocketConnection(function () {
-    msg +=';';
-    if(ws) ws.send(msg);
-    log('Message sent:' + msg);
+    waitForSocketConnection(function() {
+        msg += ';';
+        if (ws) ws.send(msg);
+        log('Message sent:' + msg);
     });
 }
